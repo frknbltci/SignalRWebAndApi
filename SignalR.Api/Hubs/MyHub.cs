ef13d7a71b945c0ca64ee363ff7c46878d5ae916
Muhammed Furkan Baltacı <fbaltaci.34@gmail.com>
2022-10-04 00:27:07 +0300

    Caller - yalnızca abone olan kişiye notify atılma aşaması

--- SignalR.Api/Hubs/MyHub.cs
+++ SignalR.Api/Hubs/MyHub.cs
@@ -8,11 +8,28 @@ namespace SignalR.Api.Hubs
 {
     public class MyHub : Hub
     {
-        public static List<string> Names { get; set; } = new List<string>();
+        private static List<string> Names { get; set; } = new List<string>();
+
+        private static int ClientCount { get; set; } = 0;
+
+        public static int TeamCount { get; set; } = 7;
+
 
         public async Task SendName(string name)
         {
-          await Clients.All.SendAsync("ReciveName", name);
+            
+
+            if (Names.Count>TeamCount)
+            {
+                await Clients.Caller.SendAsync("Error", $"Takım en fazla {TeamCount} kişi olabilir.");
+            }
+            else
+            {
+                Names.Add(name);
+                await Clients.All.SendAsync("ReceiveName", name);
+            }
+
+          
         }
 
 
@@ -21,5 +38,18 @@ namespace SignalR.Api.Hubs
             await Clients.All.SendAsync("ReceiveNames", Names);
         }
 
+        public async override Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
